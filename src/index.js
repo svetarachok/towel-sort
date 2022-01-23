72629dc9820428c7559c7562743efd4305d2d3db
Sveta Rachok <sveta.r1212@gmail.com>
2022-01-23 22:31:18 +0300

    feat: Towel sort done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix = []) {
+
+  let result = [];
+    
+  if (matrix.length == 0) {
+      result = [];
+    } else {
+      for (let index in matrix) {
+        if (index % 2 != 0) {
+          matrix[index] = matrix[index].reverse()
+        };
+      }  
+  
+      result = matrix.reduce((acc, item) => acc.concat(item));
+  } 
+  
+  return result
+  }
